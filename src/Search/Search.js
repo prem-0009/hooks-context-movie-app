fa9a1ee24f163b344b74cf3283219898ff1a9566
prem <prem.sherpa@codeimmersives.com>
2020-12-13 17:16:40 -0500

    use context

---
+++ src/Search/Search.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import SearchList from "./SearchList";
+import { MoviesContext } from "../context/MovieContext";//for context
+
+const Search = () => {
+  const fromCon = useContext(MoviesContext);//for context
+
+  const { searchValue, fetchMovieListAPI, movieResults } = fromCon;//for context
+
+  return (
+    <div>
+      <input
+        value={searchValue}
+        onChange={(e) => fetchMovieListAPI(e.target.value)}
+        type="text"
+      />
+
+      {/* { searchValue !== "" ? <SearchList movieResults={movieResults} /> : ""} */}
+      {searchValue !== "" && (
+        <SearchList 
+        // movieResults={movieResults}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/Search/SearchList.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../context/MovieContext";
+import "./Search.css";
+
+const SearchList = () => {
+  const fromCon = useContext(MoviesContext); //for context
+
+  const { searchValue, fetchMovieListAPI, movieResults } = fromCon; //for context
+
+  function showMovieList() {
+    return movieResults.map((item, i) => {
+      return (
+        <div className="search-list" key={i}>
+          <div key={i} >
+            <img
+              src={item.Poster}
+              alt="some good movie"
+              style={{ width: "200px" }}
+            />
+            {/* {console.log(i)} */}
+          </div>
+         </div>
+      );
+    });
+  }
+
+  return <ul>{showMovieList()}</ul>;
+};
+
+export default SearchList;
